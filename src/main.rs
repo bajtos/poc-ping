2907772b52915d537b74f2a7b1f24f43a7a7610f
Miroslav Bajtoš <oss@bajtos.net>
2025-04-25 15:40:24 +0200

    receive & parse ICMP ECHO response
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Result};
-use etherparse::{IcmpEchoHeader, Icmpv4Header, Icmpv4Type};
+use etherparse::{IcmpEchoHeader, Icmpv4Header, Icmpv4Slice, Icmpv4Type, Ipv4HeaderSlice};
 use rand::random;
 use socket2::{Domain, Protocol, Socket, Type};
 use std::{
@@ -19,7 +19,12 @@ fn main() -> anyhow::Result<()> {
         .parse::<Ipv4Addr>()
         .context("Failed to parse destination address")?;
 
-    let request = build_ping_request(random(), 1).unwrap();
+    let echo_id = random();
+    let echo_seq = 1;
+
+    // --- SEND ICMP ECHO REQUEST PACKET --//
+
+    let request = build_ping_request(echo_id, echo_seq).unwrap();
     println!("ICMP Ping Request packet:");
     hexdump(&request);
 
@@ -28,9 +33,6 @@ fn main() -> anyhow::Result<()> {
     // See https://apple.stackexchange.com/a/312861/51077
     let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::ICMPV4))
         .context("Failed to create socket")?;
-    socket
-        .set_recv_buffer_size(512)
-        .context("Failed to set receive buffer size")?;
 
     socket
         .bind(&SocketAddrV4::new(src, 0).into())
@@ -40,17 +42,64 @@ fn main() -> anyhow::Result<()> {
         .send_to(&request, &SocketAddrV4::new(dst, 0).into())
         .context("Failed to send ICMP Ping Request packet")?;
 
+    // --- RECEIVE ICMP ECHO RESPONSE PACKET --//
+
     socket
         .set_read_timeout(Some(Duration::from_secs(1)))
         .context("Failed to set read timeout")?;
 
-    let mut buffer = Vec::<u8>::with_capacity(512);
+    let mut buffer = Vec::<u8>::with_capacity(64_000);
     let (size, addr) = socket
         .recv_from(&mut buffer.spare_capacity_mut())
         .context("Failed to receive ICMP Ping Reply packet")?;
+    // SAFETY: recv_from wrote "size" number of bytes into the buffer
+    unsafe {
+        buffer.set_len(size);
+    }
 
-    println!("ICMP Ping Reply packet from {addr:?}:");
+    println!(
+        "ICMP Ping Reply packet from {:?} ({size} (0x{size:02x}) bytes):",
+        addr.as_socket()
+    );
     hexdump(&buffer[..size]);
+
+    // decode the header
+    let header =
+        Ipv4HeaderSlice::from_slice(&buffer).context("Failed to parse response IPv4 header")?;
+    println!(
+        "IPv4 response header: payload_len={:?}",
+        header.payload_len()
+    );
+
+    // For some reasons, the packet bytes returned by `recv_from` on macOS
+    // have the payload length incorrectly set to 8172 bytes
+    // As a result, we cannot use Ipv4Slice::from_slice(&buffer) to parse the packet
+    // let packet = Ipv4Slice::from_slice(&buffer).context("Failed to parse response packet")?;
+    // println!("ICMP response packet: {:?}", packet.payload());
+
+    let header_length = header.slice().len();
+    let response_slice = Icmpv4Slice::from_slice(&buffer[header_length..])
+        .context("Failed to parse ICMPv4 Response header")?;
+
+    println!(
+        "ICMP Ping Response: type={} code={} type={:?}",
+        response_slice.type_u8(),
+        response_slice.code_u8(),
+        response_slice.icmp_type()
+    );
+
+    match response_slice.icmp_type() {
+        Icmpv4Type::EchoReply(echo_reply) => {
+            println!(
+                "echo id match? {}, seq match? {}",
+                echo_reply.id == echo_id,
+                echo_reply.seq == echo_seq
+            );
+        }
+        _ => {
+            println!("Unexpected ICMP type: {:?}", response_slice.icmp_type());
+        }
+    }
 
     Ok(())
 }
@@ -59,6 +108,7 @@ fn build_ping_request(id: u16, seq: u16) -> Result<Vec<u8>> {
     let payload = random::<Token>();
 
     let echo_header = IcmpEchoHeader { id, seq };
+    println!("ICMP Echo Request: {echo_header:?}");
     let icmpv4_echo = Icmpv4Header::with_checksum(Icmpv4Type::EchoRequest(echo_header), &payload);
 
     let mut request = icmpv4_echo.to_bytes().to_vec();
